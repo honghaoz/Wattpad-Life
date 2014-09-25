2d9c23409775a1d3fa66cf589bd7e28188b25d58
Honghao Zhang <zhh358@gmail.com>
2014-09-25 14:21:25 -0400

    Base project

--- Wattpad Life/AppDelegate.swift
+++ Wattpad Life/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.backgroundColor = UIColor.whiteColor()
+        
+        var rootViewController: UIViewController = UIViewController.viewControllerInStoryboard("Main", viewControllerName: "MainViewController")
+        
+        self.window!.rootViewController = rootViewController
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
--- Wattpad Life/MainViewController.swift
+++ Wattpad Life/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Wattpad Life
 //
 //  Created by Honghao Zhang on 2014-09-25.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
